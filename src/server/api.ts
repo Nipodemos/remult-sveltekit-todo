a945631186dd1baefbd9e48b1dd2428e1ca5a847
Nipodemos <nipodemos13@gmail.com>
2025-07-29 11:04:14 -0300

    Update api.ts

--- src/server/api.ts
+++ src/server/api.ts
@@ -21,7 +21,9 @@ export const api = remultApi({
 			return createPostgresDataProvider({
 				connectionString: DATABASE_URL,
 				configuration: {
-					ssl: true
+					ssl: {
+					    rejectUnauthorized: false
+				        }
 				}
 			});
 		}
